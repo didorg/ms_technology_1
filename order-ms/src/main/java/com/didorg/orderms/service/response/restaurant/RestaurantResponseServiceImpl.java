d19db51cbadd313e400ee94696d24caa548a4572
Didier <didorg@gmail.com>
2020-03-30 11:35:39 -0400

    REST call to restaurant-ms & customer-ms using Dynamic Configuration

--- order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
+++ order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
@@ -2,6 +2,7 @@ package com.didorg.orderms.service.response.restaurant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,13 +13,16 @@ import com.didorg.orderms.dto.api.restaurant.Restaurant;
 public class RestaurantResponseServiceImpl implements IRestaurantResponseService{
 	private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantResponseServiceImpl.class);
 	private final RestTemplate restTemplate;
+	@Value("${restaurant.rest.host}")
+    private String restaurantHostUrl;
 
 	public RestaurantResponseServiceImpl(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public Restaurant getRestaurantById(String id) {
-		ResponseEntity<Restaurant> entity = restTemplate.getForEntity("http://localhost:8040/restaurants/" + id,
+		LOGGER.info("GET to Restaurant REST HOST URL --> "+restaurantHostUrl);
+		ResponseEntity<Restaurant> entity = restTemplate.getForEntity(restaurantHostUrl + id,
 				Restaurant.class);
 		LOGGER.info("Status code value: " + entity.getStatusCodeValue());
 		LOGGER.info("HTTP Header 'ContentType': " + entity.getHeaders().getContentType());
