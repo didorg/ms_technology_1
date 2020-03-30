bac7f6b5c9e938ec2f3398d05cb72f160fbe2ced
Didier <didorg@gmail.com>
2020-03-30 13:10:51 -0400

    add @RefreshScope

--- order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
+++ order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
@@ -3,12 +3,14 @@ package com.didorg.orderms.service.response.restaurant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.didorg.orderms.dto.api.restaurant.Restaurant;
 
+@RefreshScope
 @Service
 public class RestaurantResponseServiceImpl implements IRestaurantResponseService{
 	private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantResponseServiceImpl.class);
