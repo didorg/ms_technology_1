a763a29ff4f2e9b82435f1d4a22bcc0c743467eb
Didier <didorg@gmail.com>
2020-03-31 16:21:00 -0400

    implement Load balance requests between microservices using Ribbon

--- order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
+++ order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,8 +13,11 @@ import com.didorg.orderms.dto.api.restaurant.Restaurant;
 
 @RefreshScope
 @Service
+@RibbonClient(name = "restaurantRibbon")
 public class RestaurantResponseServiceImpl implements IRestaurantResponseService{
 	private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantResponseServiceImpl.class);
+	private static final String RIBBON = "http://restaurantRibbon/";
+	private static final String RESTAURANT = "restaurants/";
 	private final RestTemplate restTemplate;
 	@Value("${restaurant.rest.host}")
     private String restaurantHostUrl;
@@ -23,8 +27,8 @@ public class RestaurantResponseServiceImpl implements IRestaurantResponseService
 	}
 
 	public Restaurant getRestaurantById(String id) {
-		LOGGER.info("GET to Restaurant REST HOST URL --> "+restaurantHostUrl);
-		ResponseEntity<Restaurant> entity = restTemplate.getForEntity(restaurantHostUrl + id,
+		LOGGER.info("GET to Restaurant REST HOST URL --> "+RIBBON + RESTAURANT + id);
+		ResponseEntity<Restaurant> entity = restTemplate.getForEntity(RIBBON + RESTAURANT + id,
 				Restaurant.class);
 		LOGGER.info("Status code value: " + entity.getStatusCodeValue());
 		LOGGER.info("HTTP Header 'ContentType': " + entity.getHeaders().getContentType());
