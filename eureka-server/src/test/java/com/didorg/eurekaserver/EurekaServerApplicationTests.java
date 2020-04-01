20f77f69084870f7d12791468ab680b6831d2f1e
Didier <didorg@gmail.com>
2020-04-01 11:38:48 -0400

    add Eureka Server

---
+++ eureka-server/src/test/java/com/didorg/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.didorg.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
