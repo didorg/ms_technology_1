90ae4d577054f081ad6552ea04aaa88100e1f674
Didier <didorg@gmail.com>
2020-04-01 13:21:49 -0400

    Ribbon with Eureka

--- order-ms/src/main/java/com/didorg/orderms/service/response/customer/CustomerResponseServiceImpl.java
+++ order-ms/src/main/java/com/didorg/orderms/service/response/customer/CustomerResponseServiceImpl.java
@@ -2,9 +2,7 @@ package com.didorg.orderms.service.response.customer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,24 +11,20 @@ import com.didorg.orderms.dto.api.customer.Customer;
 
 @RefreshScope
 @Service
-@RibbonClient(name = "customerRibbon")
-public class CustomerResponseServiceImpl implements ICustomerResponseService{
+public class CustomerResponseServiceImpl implements ICustomerResponseService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomerResponseServiceImpl.class);
-	private static final String RIBBON = "http://customerRibbon/";
+	private static final String RIBBON = "http://customerMS/";
 	private static final String CUSTOMER = "customers/";
-	
+
 	private final RestTemplate restTemplate;
-	@Value("${customer.rest.host}")
-    private String customertHostUrl;
 
 	public CustomerResponseServiceImpl(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public Customer getCustomerById(String id) {
-		LOGGER.info("GET to Customer REST HOST URL --> "+RIBBON + CUSTOMER + id);
-		ResponseEntity<Customer> entity = restTemplate.getForEntity(RIBBON + CUSTOMER + id,
-				Customer.class);
+		LOGGER.info("GET to Customer REST HOST URL --> " + RIBBON + CUSTOMER + id);
+		ResponseEntity<Customer> entity = restTemplate.getForEntity(RIBBON + CUSTOMER + id, Customer.class);
 		LOGGER.info("Status code value: " + entity.getStatusCodeValue());
 		LOGGER.info("HTTP Header 'ContentType': " + entity.getHeaders().getContentType());
 
--- order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
+++ order-ms/src/main/java/com/didorg/orderms/service/response/restaurant/RestaurantResponseServiceImpl.java
@@ -2,9 +2,7 @@ package com.didorg.orderms.service.response.restaurant;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -13,23 +11,19 @@ import com.didorg.orderms.dto.api.restaurant.Restaurant;
 
 @RefreshScope
 @Service
-@RibbonClient(name = "restaurantRibbon")
-public class RestaurantResponseServiceImpl implements IRestaurantResponseService{
+public class RestaurantResponseServiceImpl implements IRestaurantResponseService {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RestaurantResponseServiceImpl.class);
-	private static final String RIBBON = "http://restaurantRibbon/";
+	private static final String RIBBON = "http://restaurantMS/";
 	private static final String RESTAURANT = "restaurants/";
 	private final RestTemplate restTemplate;
-	@Value("${restaurant.rest.host}")
-    private String restaurantHostUrl;
 
 	public RestaurantResponseServiceImpl(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
 	}
 
 	public Restaurant getRestaurantById(String id) {
-		LOGGER.info("GET to Restaurant REST HOST URL --> "+RIBBON + RESTAURANT + id);
-		ResponseEntity<Restaurant> entity = restTemplate.getForEntity(RIBBON + RESTAURANT + id,
-				Restaurant.class);
+		LOGGER.info("GET to Restaurant REST HOST URL --> " + RIBBON + RESTAURANT + id);
+		ResponseEntity<Restaurant> entity = restTemplate.getForEntity(RIBBON + RESTAURANT + id, Restaurant.class);
 		LOGGER.info("Status code value: " + entity.getStatusCodeValue());
 		LOGGER.info("HTTP Header 'ContentType': " + entity.getHeaders().getContentType());
 
